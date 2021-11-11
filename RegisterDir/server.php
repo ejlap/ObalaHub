c87dddb12d8b6e663f1667e0a4a037f6f3d563ec
kaisershbzt <93866204+kaisershbzt@users.noreply.github.com>
2021-11-11 19:39:14 +0100

    Removed comments

--- RegisterDir/server.php
+++ RegisterDir/server.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
 
-// initializing variables
+
 $name="";
 $surname="";
 $razred="";
@@ -12,12 +12,12 @@ $user_name ="";
 $user_mail ="";
 $errors = array(); 
 
-// connect to the database
+
 $db = mysqli_connect('localhost', 'root', '', 'registration');
 
-// REGISTER USER
+
 if (isset($_POST['reg_user'])) {
-  // receive all input values from the form
+
   $name = mysqli_real_escape_string($db, $_POST['name']);
   $surname = mysqli_real_escape_string($db, $_POST['surname']);
   $razred = mysqli_real_escape_string($db, $_POST['razred']);
@@ -26,8 +26,7 @@ if (isset($_POST['reg_user'])) {
   $email = mysqli_real_escape_string($db, $_POST['email']);
   $password_1 = mysqli_real_escape_string($db, $_POST['password_1']);
   $password_2 = mysqli_real_escape_string($db, $_POST['password_2']);
-  // form validation: ensure that the form is correctly filled ...
-  // by adding (array_push()) corresponding error unto $errors array
+
   if (empty($name)) { array_push($errors, "Name is required"); }
   if (empty($surname)) { array_push($errors, "Surname is required"); }
   if (empty($razred)) { array_push($errors, "Razred is required"); }
@@ -39,8 +38,8 @@ if (isset($_POST['reg_user'])) {
   array_push($errors, "The two passwords do not match");
   }
 
-  // first check the database to make sure 
-  // a user does not already exist with the same username and/or email
+
+
   $user_check_query = "SELECT * FROM users WHERE username='$username' OR email='$email' LIMIT 1";
   $result = mysqli_query($db, $user_check_query);
   $user = mysqli_fetch_assoc($result);
@@ -55,9 +54,9 @@ if (isset($_POST['reg_user'])) {
     }
   }
 
-  // Finally, register user if there are no errors in the form
+
   if (count($errors) == 0) {
-    $password = md5($password_1);//encrypt the password before saving in the database
+    $password = md5($password_1);
 
     $query = "INSERT INTO users (username, email, password, name, surname, razred, odjeljenje) 
           VALUES('$username', '$email', '$password', '$name', '$surname', '$razred','$odjeljenje')";
